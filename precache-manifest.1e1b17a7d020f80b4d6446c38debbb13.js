e3f8138c345ad1781e406997b570a4048f92e952
skand <skand.mishra@mobicules.com>
2020-03-18 19:54:05 +0530

    Updates

---
+++ precache-manifest.1e1b17a7d020f80b4d6446c38debbb13.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "356992e73862ae15e931",
+    "url": "/portfolio/static/css/main.3ab01a06.chunk.css"
+  },
+  {
+    "revision": "356992e73862ae15e931",
+    "url": "/portfolio/static/js/main.356992e7.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.53ebc025.js"
+  },
+  {
+    "revision": "5711b84e88abdf95a81e",
+    "url": "/portfolio/static/js/2.5711b84e.chunk.js"
+  },
+  {
+    "revision": "316237141a0f258680026721fd4af0ff",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.40713636bde8b32b0f7c1f1caced417b.js"
+  "/portfolio/precache-manifest.1e1b17a7d020f80b4d6446c38debbb13.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/portfolio/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
