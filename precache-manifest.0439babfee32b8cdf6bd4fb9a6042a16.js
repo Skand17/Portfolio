cb5ede53b8a5e4d1dd25b3e618202d9c4bebbb35
skand <skand.mishra@mobicules.com>
2020-04-19 14:49:13 +0530

    Updates

---
+++ precache-manifest.0439babfee32b8cdf6bd4fb9a6042a16.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "272d8412f4994be2de84",
+    "url": "/static/css/main.97815108.chunk.css"
+  },
+  {
+    "revision": "272d8412f4994be2de84",
+    "url": "/static/js/main.272d8412.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "a3acd653b8edf189d15d",
+    "url": "/static/css/2.d9cd6eb6.chunk.css"
+  },
+  {
+    "revision": "a3acd653b8edf189d15d",
+    "url": "/static/js/2.a3acd653.chunk.js"
+  },
+  {
+    "revision": "55e9c63c701585636f050b1c05d7fd94",
+    "url": "/static/media/oaks.55e9c63c.png"
+  },
+  {
+    "revision": "feaffd5cc8c3a1984553379c45c8e036",
+    "url": "/static/media/janeous.feaffd5c.png"
+  },
+  {
+    "revision": "5bdf73d34d7a0c0f48a29ec7cfcd51fd",
+    "url": "/static/media/obuhi.5bdf73d3.png"
+  },
+  {
+    "revision": "2068d8f4d55b5b5ae278faea415517da",
+    "url": "/static/media/seed.2068d8f4.png"
+  },
+  {
+    "revision": "ab36c67138d7b19a95620cad77f613a1",
+    "url": "/static/media/profile.ab36c671.jpg"
+  },
+  {
+    "revision": "ba0e63b27f4507e0cb01d1ce0a1c3884",
+    "url": "/static/media/react.ba0e63b2.png"
+  },
+  {
+    "revision": "9b05003ff4b78eda700b7c08f08cb53e",
+    "url": "/index.html"
+  }
+];
